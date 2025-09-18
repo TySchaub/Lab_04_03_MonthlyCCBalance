4366ba9ab5bb677c97385bf5d1926af4ffea7215
Ty Schaub <tyschaub@Tys-MacBook-Air.local>
2025-09-18 15:07:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    {
+        int ccBalance = 5000;
+        double interestRate = 0.17;
+        
+        double interest = ccBalance * interestRate;
+        double firstMonth = ccBalance + interest;
+        double secondMonth = firstMonth + interest;
+
+        System.out.println("Your first month account balance is: " + firstMonth);
+        System.out.println("Your second month account balance is: " + secondMonth);
+
+    }
+}
